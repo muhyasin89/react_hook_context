520c6d2558cb4c9ca5f041b1b672daeb3768b4a4
muhyasin89 <muhyasin89@gmail.com>
2020-07-22 13:39:48 +0700

    [UPDATE]change context

---
+++ hooks/src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SongList = () => {
+     return (
+         <div className="song-list">
+            <ul>
+                <li> peterpan--Semua Tentang Kita </li>
+                <li> peterpan-- Mimpi Yang Sempurna </li>
+                <li> peterpan--Bintang di Surga </li>
+            </ul>
+         </div>
+     );
+}
+
+export default SongList;
