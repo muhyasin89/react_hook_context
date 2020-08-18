e1e1f543ae4e668d7168962bf519bf4a3f04879c
muhyasin89 <muhyasin89@gmail.com>
2020-08-19 06:37:44 +0700

    [UPDATE]consuming multiple context

--- context/src/App.js
+++ context/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
+import ThemeToggle from './components/ThemeToggle';
 import NavigationBar from './components/NavigationBar';
 import BookList from './components/pages/book/BookList';
 import ThemeContextProvider from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
-
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
   return (
       <ThemeContextProvider>
-          <NavigationBar />
-          <BookList />
-          <ThemeToggle />
+        <AuthContextProvider>
+            <NavigationBar />
+            <BookList />
+            <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>  
   );
 }
