92b8bc505de6be8cee1da2b47f97a1e6736d1cb8
muhyasin89 <muhyasin89@gmail.com>
2020-08-19 06:01:27 +0700

    [UPDATE]change to basic

--- context/src/App.js
+++ context/src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
-
 import NavigationBar from './components/NavigationBar';
 import BookList from './components/pages/book/BookList';
 import BookAddForm from './components/pages/book/BookAddForm';
@@ -16,19 +10,10 @@ import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
-    <React.Fragment>
       <ThemeContextProvider>
           <NavigationBar />
-        <Router>
-            <Switch>
-              <Route exact path="/" component={BookList} />
-              <Route exact path="/add" component={BookAddForm} />
-              <Route exact path="/delete" component={BookDelete} />
-              <Route component={status404} />
-            </Switch>
-          </Router>
+          <BookList />
       </ThemeContextProvider>  
-    </React.Fragment>    
   );
 }
 
--- context/src/components/NavigationBar.js
+++ context/src/components/NavigationBar.js
@@ -1,58 +1,12 @@
 import React, { Component } from 'react';
 
-import styled from 'styled-components';
-
-import {
-    Nav,
-    Navbar,
-    NavDropdown,
-    Form,
-    FormControl,
-    Button
-} from 'react-bootstrap';
-
-import ThemeContext from '../contexts/ThemeContext';
-
-
-
 class NavigationBar extends Component {
-    static contextType = ThemeContext;
-    
-    render() {
-        console.log(this.context);
-        const { isLightTheme, light, dark } = this.context;
-        const theme =  isLightTheme ? light : dark;
-    
-        // const Styles = styled.div`
-        //     #nav{
-        //         background: ${theme.ui},
-        //         color: ${theme.syntax}
-        //     }
-        // `;
+
+
+    render(){
         return (
-        <Navbar bg="light" expand="lg" >
-            <Navbar.Brand href = "#home" > React - Bootstrap </Navbar.Brand> 
-            <Navbar.Toggle aria-controls = "basic-navbar-nav" />
-            <Navbar.Collapse id = "basic-navbar-nav">
-                <Nav className = "mr-auto" >
-                    <Nav.Link href = "/" > Home </Nav.Link>
-                    <Nav.Link href = "/about" > About </Nav.Link>
-                    <Nav.Link href = "/contact" > Contact </Nav.Link>  
-                    <NavDropdown title = "Dropdown" id = "basic-nav-dropdown" >
-                    <NavDropdown.Item href = "#action/3.1" > Action </NavDropdown.Item> 
-                    <NavDropdown.Item href = "#action/3.2" > Another action </NavDropdown.Item> 
-                    <NavDropdown.Item href = "#action/3.3" > Something </NavDropdown.Item> 
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href = "#action/3.4" > Separated link </NavDropdown.Item> 
-                    </NavDropdown> 
-                </Nav> 
-                <Form inline >
-                <FormControl type = "text" placeholder = "Search" className = "mr-sm-2" />
-                <Button variant = "outline-success" > Search </Button> 
-                </Form> 
-            </Navbar.Collapse> 
-        </Navbar>
-        );
+            <h1> Navigation</h1>
+        )
     }
 }
 
