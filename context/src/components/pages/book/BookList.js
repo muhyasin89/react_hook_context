911ff38cbca28181b938e6936e1df5daae8dbd7c
muhyasin89 <muhyasin89@gmail.com>
2020-08-19 10:06:58 +0700

    [UPDATE]change book list from component into context

--- context/src/components/pages/book/BookList.js
+++ context/src/components/pages/book/BookList.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../../../contexts/ThemeContext';
 
 
-class BookList extends Component{
-    static contextType = ThemeContext;
-    render(){
-        const  { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className='book-list'>
-                <ul>
-                    <li>the art of war</li>
-                    <li>the atomic habit</li>
-                    <li>The habit of effective people</li>
-                </ul>
-            </div>
-        );
-    }
+const BookList = () => {
+    const  { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+
+    return (
+        <div className='book-list' style={{ color: theme.syntax, background: theme.bg }}>
+            <ul>
+                <li style={{ background: theme.ui }}>the art of war</li>
+                <li style={{ background: theme.ui }}>the atomic habit</li>
+                <li style={{ background: theme.ui }}>The habit of effective people</li>
+            </ul>
+        </div>
+    );
 }
 
 
